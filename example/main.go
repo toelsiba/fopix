f4e1cc552ea2ec54d43cd8ae26420fdf16e0433a
Chander <ChanderWork112358@gmail.com>
2016-06-01 11:09:52 +0300

    add samples

--- example/main.go
+++ example/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"bufio"
 	"image"
-	"image/color"
-	"image/draw"
 	"image/png"
 	"log"
 	"os"
@@ -12,30 +10,24 @@ import (
 	"github.com/toelsiba/fopix"
 )
 
-const (
-	font3x3         = "../fonts/font3x3.json"
-	fontVictor      = "../fonts/victor.json"
-	fontMiniwi      = "../fonts/miniwi.json"
-	fontTomThumb    = "../fonts/tom-thumb.json"
-	fontTomThumbNew = "../fonts/tom-thumb-new.json"
-	fontDigits3x3   = "../fonts/digits3x3.json"
-	fontDigits3x4   = "../fonts/digits3x4.json"
-	fontDigits3x5   = "../fonts/digits3x5.json"
-	fontPixefon4x5  = "../fonts/pixefon-4x5.json"
-)
+func main() {
 
-var textMultiline = `During the 20th century, the field of professional astronomy
-split into observational and theoretical branches. Observational astronomy
-is focused on acquiring data from observations of astronomical objects, which
-is then analyzed using basic principles of physics. Theoretical astronomy is
-oriented toward the development of computer or analytical models to describe
-astronomical objects and phenomena. The two fields complement each other, with
-theoretical astronomy seeking to explain the observational results and
-observations being used to confirm theoretical results.
-Astronomy is one of the few sciences where amateurs can still play an active
-role, especially in the discovery and observation of transient phenomena.
-Amateur astronomers have made and contributed to many important astronomical
-discoveries.`
+	f, err := fopix.NewFromFile("../fonts/tom-thumb-new.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	f.Scale(5)
+
+	const text = "Hello, World!"
+
+	m := image.NewRGBA(f.GetTextBounds(text))
+
+	f.DrawText(m, image.ZP, text)
+
+	if err = imageSaveToPNG("test.png", m); err != nil {
+		log.Fatal(err)
+	}
+}
 
 func imageSaveToPNG(fileName string, i image.Image) error {
 
@@ -49,126 +41,4 @@ func imageSaveToPNG(fileName string, i image.Image) error {
 	defer w.Flush()
 
 	return png.Encode(w, i)
-}
-
-func fillImage(m *image.RGBA, c color.Color) {
-	var (
-		data = m.Pix
-		q    = color.RGBAModel.Convert(c).(color.RGBA)
-		temp = []uint8{q.R, q.G, q.B, q.A}
-	)
-	for len(data) >= 4 {
-		copy(data, temp)
-		data = data[4:]
-	}
-}
-
-func test1() {
-
-	f, err := fopix.NewFromFile(fontPixefon4x5)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	f.Scale(2)
-	f.Color(color.RGBA{0, 0, 0xFF, 0xFF})
-
-	//text := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	//text := "abcdefghijklmnopqrstuvwxyz"
-	//text := "`1234567890-=[]\\;',./"
-	//text := "~!@#$%^&*()_+{}|:\"<>?"
-	text := textMultiline
-	//text := "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"
-	//text := "A1B2C3D4E5F6G7H8I9J"
-	//text := "a1b2c3d4e5f6g7h8i9j"
-
-	bounds := f.GetTextBounds(text)
-	if bounds.Empty() {
-		log.Fatal("bounds is empty")
-	}
-	m := image.NewRGBA(bounds)
-
-	//fillImage(m, color.Black)
-	fillImage(m, color.RGBA{50, 50, 50, 255})
-
-	f.DrawText(m, image.Point{0, 0}, text)
-
-	err = imageSaveToPNG("test.png", m)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func test2() {
-
-	m1 := image.NewRGBA(image.Rect(0, 0, 300, 100))
-
-	bgColor := color.RGBA{255, 255, 255, 255}
-	draw.Draw(m1, m1.Bounds(), &image.Uniform{bgColor}, image.ZP, draw.Src)
-
-	text := "Hello, World!"
-
-	f, err := fopix.NewFromFile(fontTomThumbNew)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	f.Scale(5)
-	f.Color(color.RGBA{0, 0, 255, 100})
-
-	m2 := image.NewRGBA(f.GetTextBounds(text))
-
-	f.DrawText(m2, image.Point{0, 0}, text)
-
-	draw.Draw(m1, m1.Bounds(), m2, image.Point{0, 0}, draw.Over)
-
-	cc := color.RGBAModel.Convert(m1.At(0, 6)).(color.RGBA)
-	log.Println(cc)
-
-	err = imageSaveToPNG("test.png", m1)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func drawASCIITable() {
-
-	var data []byte
-	for y := 1; y < 4; y++ {
-		if y > 1 {
-			data = append(data, '\n')
-		}
-		for x := 0; x < 32; x++ {
-			data = append(data, byte(y*32+x))
-		}
-	}
-	text := string(data)
-
-	fontFile := fontVictor
-
-	f, err := fopix.NewFromFile(fontFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	f.Color(color.RGBA{0, 0, 0xFF, 0xFF})
-	f.Scale(3)
-
-	bounds := f.GetTextBounds(text)
-	if bounds.Empty() {
-		log.Fatal("bounds is empty")
-	}
-	m := image.NewRGBA(bounds)
-
-	fillImage(m, color.RGBA{50, 50, 50, 255})
-
-	f.DrawText(m, image.Point{0, 0}, text)
-
-	err = imageSaveToPNG("test.png", m)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func main() {
-	drawASCIITable()
 }
---
+++ samples/main.go
@@ -0,0 +1,205 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/toelsiba/fopix"
+)
+
+type Sample struct {
+	FontFile  string
+	Text      string
+	ImageFile string
+	Scale     int
+}
+
+func makeSample(s Sample, outDir string) error {
+
+	f, err := fopix.NewFromFile(s.FontFile)
+	if err != nil {
+		return err
+	}
+
+	f.Scale(s.Scale)
+	f.Color(color.RGBA{0, 0, 0xFF, 0xFF})
+
+	bounds := f.GetTextBounds(s.Text)
+	if bounds.Empty() {
+		return errors.New("bounds is empty")
+	}
+	m := image.NewRGBA(bounds)
+
+	fillImage(m, color.RGBA{0x24, 0x24, 0x24, 0xFF})
+
+	f.DrawText(m, image.ZP, s.Text)
+
+	return imageSaveToPNG(filepath.Join(outDir, s.ImageFile), m)
+}
+
+func fillImage(m draw.Image, c color.Color) {
+	draw.Draw(m, m.Bounds(), &image.Uniform{c}, image.ZP, draw.Src)
+}
+
+func imageSaveToPNG(fileName string, i image.Image) error {
+
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	w := bufio.NewWriter(file)
+	defer w.Flush()
+
+	return png.Encode(w, i)
+}
+
+func drawableASCII() string {
+	var data []byte
+	for y := 1; y < 4; y++ {
+		if y > 1 {
+			data = append(data, '\n')
+		}
+		for x := 0; x < 32; x++ {
+			data = append(data, byte(y*32+x))
+		}
+	}
+	return string(data)
+}
+
+func main() {
+
+	const (
+		fontFile3x3         = "../fonts/font-3x3.json"
+		fontFileVictor      = "../fonts/victor.json"
+		fontFileMiniwi      = "../fonts/miniwi.json"
+		fontFileTomThumb    = "../fonts/tom-thumb.json"
+		fontFileTomThumbNew = "../fonts/tom-thumb-new.json"
+		fontFileDigits3x3   = "../fonts/digits-3x3.json"
+		fontFileDigits3x4   = "../fonts/digits-3x4.json"
+		fontFileDigits3x5   = "../fonts/digits-3x5.json"
+		fontFilePixefon4x5  = "../fonts/pixefon-4x5.json"
+	)
+
+	var textMultiline = `During the 20th century, the field of professional astronomy
+split into observational and theoretical branches. Observational astronomy
+is focused on acquiring data from observations of astronomical objects, which
+is then analyzed using basic principles of physics. Theoretical astronomy is
+oriented toward the development of computer or analytical models to describe
+astronomical objects and phenomena. The two fields complement each other, with
+theoretical astronomy seeking to explain the observational results and
+observations being used to confirm theoretical results.
+Astronomy is one of the few sciences where amateurs can still play an active
+role, especially in the discovery and observation of transient phenomena.
+Amateur astronomers have made and contributed to many important astronomical
+discoveries.`
+
+	var textASCII = drawableASCII()
+
+	samples := []Sample{
+		Sample{
+			FontFile:  fontFileDigits3x3,
+			Text:      "0123456789",
+			ImageFile: "digits-3x3.png",
+			Scale:     10,
+		},
+		Sample{
+			FontFile:  fontFileDigits3x4,
+			Text:      "0123456789",
+			ImageFile: "digits-3x4.png",
+			Scale:     10,
+		},
+		Sample{
+			FontFile:  fontFileDigits3x5,
+			Text:      "0123456789",
+			ImageFile: "digits-3x5.png",
+			Scale:     10,
+		},
+		Sample{
+			FontFile:  fontFile3x3,
+			Text:      textASCII,
+			ImageFile: "font-3x3-ascii.png",
+			Scale:     5,
+		},
+		Sample{
+			FontFile:  fontFile3x3,
+			Text:      textMultiline,
+			ImageFile: "font-3x3-multiline.png",
+			Scale:     2,
+		},
+		Sample{
+			FontFile:  fontFileTomThumb,
+			Text:      textASCII,
+			ImageFile: "tom-thumb-ascii.png",
+			Scale:     5,
+		},
+		Sample{
+			FontFile:  fontFileTomThumb,
+			Text:      textMultiline,
+			ImageFile: "tom-thumb-multiline.png",
+			Scale:     2,
+		},
+		Sample{
+			FontFile:  fontFileTomThumbNew,
+			Text:      textASCII,
+			ImageFile: "tom-thumb-new-ascii.png",
+			Scale:     5,
+		},
+		Sample{
+			FontFile:  fontFileTomThumbNew,
+			Text:      textMultiline,
+			ImageFile: "tom-thumb-new-multiline.png",
+			Scale:     2,
+		},
+		Sample{
+			FontFile:  fontFileVictor,
+			Text:      textASCII,
+			ImageFile: "victor-ascii.png",
+			Scale:     3,
+		},
+		Sample{
+			FontFile:  fontFileVictor,
+			Text:      textMultiline,
+			ImageFile: "victor-multiline.png",
+			Scale:     1,
+		},
+		Sample{
+			FontFile:  fontFileMiniwi,
+			Text:      textASCII,
+			ImageFile: "miniwi-ascii.png",
+			Scale:     5,
+		},
+		Sample{
+			FontFile:  fontFileMiniwi,
+			Text:      textMultiline,
+			ImageFile: "miniwi-multiline.png",
+			Scale:     2,
+		},
+		Sample{
+			FontFile:  fontFilePixefon4x5,
+			Text:      textASCII,
+			ImageFile: "pixefon-4x5-ascii.png",
+			Scale:     5,
+		},
+		Sample{
+			FontFile:  fontFilePixefon4x5,
+			Text:      textMultiline,
+			ImageFile: "pixefon-4x5-multiline.png",
+			Scale:     2,
+		},
+	}
+
+	for _, s := range samples {
+		if err := makeSample(s, "images"); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
